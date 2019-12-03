7642c91e19fe999e6bea19ebe57964a77f36b538
nayfin <natjkern@gmail.com>
2019-12-03 10:48:44 -0600

    creates type for all possible input types

--- libs/crispr-forms/src/lib/models/input-field-config.ts
+++ libs/crispr-forms/src/lib/models/input-field-config.ts
@@ -1,5 +1,28 @@
 import { DynamicFieldConfig } from '../models';
 
 export interface InputFieldConfig extends DynamicFieldConfig {
-  inputType?: string;
+  inputType?: InputType;
 }
+
+type InputType ='button'|
+  'checkbox'|
+  'color'|
+  'date'|
+  'datetime-local'|
+  'email'|
+  'file'|
+  'hidden'|
+  'image'|
+  'month'|
+  'number'|
+  'password'|
+  'radio'|
+  'range'|
+  'reset'|
+  'search'|
+  'submit'|
+  'tel'|
+  'text'|
+  'time'|
+  'url'|
+  'week';
