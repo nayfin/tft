463b5776053208e57f7580ddab2713ee423246fe
nayfin <natjkern@gmail.com>
2019-12-21 21:26:53 -0600

    release:   "version": "8.4.2",

--- libs/crispr-forms/src/lib/models/button.config.ts
+++ libs/crispr-forms/src/lib/models/button.config.ts
@@ -2,6 +2,7 @@ import { CrisprFieldConfig } from './crispr-field.config';
 
 export interface ButtonConfig extends CrisprFieldConfig {
   buttonType: 'raised' | 'flat' | 'stroked' | 'icon'; // TODO: 'fab' || 'mini-fab'
+  color?: string;
   icon?: string;
   disabledOnInvalidForm?: boolean;
 }
