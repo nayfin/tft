d7654afeea2d51f9e0c30bad365c1f63ac884c71
nayfin <natjkern@gmail.com>
2019-12-21 21:34:21 -0600

    release:   "version": "8.4.3",

--- libs/crispr-forms/src/lib/public_api.ts
+++ libs/crispr-forms/src/lib/public_api.ts
@@ -4,5 +4,6 @@ export * from './models/select-field.config';
 export * from './models/autocomplete-field.config';
 export * from './models/form-group-list.config';
 export * from './form.helpers';
+export * from './material';
 export * from './crispr-forms.module';
 
