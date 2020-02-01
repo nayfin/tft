ff1551015ee667d32e0997348005fed01f7da05e
nayfin <natjkern@gmail.com>
2020-02-01 11:43:58 -0600

    interact release:   "version": "8.0.8",

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,46 +1,39 @@
 
 #!/bin/bash
-package=$1
-# TODO: learn how to make this not terrible and ugly
-if [ "$package" != "interact" ] && [ "$package" != "crispr-forms" ] && [ "$package" != "form-validation-handler" ] && [ "$package" != "core" ] && [ "$package" != "ui-imports" ]
-then
-  echo "Not a valid package to release, try 'interact', 'crispr-forms', or 'form-validation-handler'"
-  exit 1
-fi
+# exit when error
+set -e
+# TODO: There's probably a fancy way to automate this
+# THIS LIST MUST BE UPDATEd WHEN NEW PUBLISHABLE LIBS ARE ADDED
+packages=(interact crisper-forms form-validation-handler core ui-imports)
+# prompt user to ask what package we are releasing
+PS3='What package are we releaseing? (input number)'
+select package in "${packages[@]}"
+# We only need this prompt to set the $package variable, but bash requires the do, break, done
+do
+  break
+done
 
-set -e # exit when error
-
-# check we are on master
+# check that we are on master
 branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$branch" != "master" ]
-then
-  echo "Not on master branch, exiting"
-  exit 1
+  then
+    echo "Not on master branch, exiting"
+    exit 1
 fi
 # get current version of package being released
 actual_version=$(grep version "libs/$package/package.json")
-echo " ${actual_version}"
+echo "Current Version: ${actual_version}"
 
 # holds variable update types to check against user input
-update_options=(major minor patch)
-
-echo "What type of update is this?"
-echo "options: ${update_options[*]}"
-
-read update_type
-if [[ ! " ${update_options[@]} " =~ " ${update_type} " ]]; then
-    # whatever you want to do when arr doesn't contain value
-# fi
-# # TODO: properly check against update_options array instead of limping through this if statement
-# if [ "$update_type" != "patch" ] && [ "$update_type" != "minor" ] && [ "$update_type" != "major" ]
-# then
-  echo "Not a valid semantic update, try 'patch', 'minor', or 'major'"
-  exit 1
-fi
-
-# go into the library, bump the version according to update type then get out
-cd "libs/$package" && npm version "${update_type}" && cd ../../
-#  build the library and prepare to publish
+update_options=(patch minor major)
+PS3='What type of update is this? (input number)'
+select update_type in "${update_options[@]}"
+do
+  # go into the library, bump the version according to update type then get out
+  cd "libs/$package" && npm version "${update_type}" && cd ../../
+  break
+done
+# build the library and prepare to publish
 ng build $package
 # package the build code in dist and publish it then go back to root
 cd "dist/libs/$package" && npm pack && npm publish --access public && cd ../../../
@@ -49,5 +42,8 @@ cd "dist/libs/$package" && npm pack && npm publish --access public && cd ../../.
 release_version=$(grep version "libs/$package/package.json")
 # stage, commit and push all changes
 git add . && git commit -m "$package release: $release_version" && git push
-
 npm run release:documentation
+
+
+
+
