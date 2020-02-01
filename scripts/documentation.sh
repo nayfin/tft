6eb3e3ba92f6d67ca70d6a883856d5b14e3b0f2b
nayfin <natjkern@gmail.com>
2020-02-01 11:47:13 -0600

    updates documentation

--- scripts/documentation.sh
+++ scripts/documentation.sh
@@ -1,6 +1,5 @@
 
-release_version=$(grep peerDependencies "libs/core/package.json")
-echo $release_version
-#cp -rf ./apps/documentation/src/app ./../tft-documentation/src && cd ../tft-documentation && git add . && git commit -m 'release to stackblitz' && git push && cd ../tft
+cp -rf ./apps/documentation/src/app ./../tft-documentation/src
+cd ../tft-documentation && git add . && git commit -m 'release to stackblitz' && git push && cd ../tft
 
 # TODO: need to update @tft/dependencies in package.json of tft-documentation
