dac7f2070036f1364c8eed52590653c5058e3fab
nayfin <natjkern@gmail.com>
2020-11-03 09:38:38 -0600

    changes to alternate drag container examples

--- apps/documentation/src/app/interact-demo/features/alternate-drag-container/alternate-drag-container.component.ts
+++ apps/documentation/src/app/interact-demo/features/alternate-drag-container/alternate-drag-container.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'doc-alternate-drag-container',
   templateUrl: './alternate-drag-container.component.html',
   styleUrls: ['./alternate-drag-container.component.scss']
 })
-export class AlternateDragContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class AlternateDragContainerComponent {
 
 }
