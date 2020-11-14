047e434727615f1a0fc8e7909273104a7e04471b
nayfin <natjkern@gmail.com>
2020-11-13 22:26:39 -0600

    add a drag config and a  handle to placeholder example

--- apps/documentation/src/app/interact-demo/features/placeholder-template/placeholder-template.component.ts
+++ apps/documentation/src/app/interact-demo/features/placeholder-template/placeholder-template.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DraggableOptions } from '@interactjs/types/index';
 
 @Component({
   selector: 'tft-placeholder-template',
   templateUrl: './placeholder-template.component.html',
   styleUrls: ['./placeholder-template.component.scss']
 })
-export class PlaceholderTemplateComponent implements OnInit {
+export class PlaceholderTemplateComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  dragConfig: DraggableOptions = {allowFrom: '.handle'}
 
 }
