f7b245aa9e5e4232642e3386bfec70c7468f95b2
nayfin <natjkern@gmail.com>
2021-03-13 15:58:47 -0600

    creates discrimate union of possible upload field configs

--- libs/crispr-forms/src/lib/models/file-upload-field.config.ts
+++ libs/crispr-forms/src/lib/models/file-upload-field.config.ts
@@ -1,19 +1,32 @@
 import { FormGroup } from '@angular/forms';
 import { FileUploadComponent } from '../material';
-import { CrisprControlConfig, ControlType, ButtonType } from '../models';
-// This is done different from the rest of the fields because
-// when we use the heading inside of a field we don't want to have to
-// pass a controlType
-export interface FileUploadFieldConfig extends CrisprControlConfig {
+import { CrisprControlConfig, ControlType, ButtonType, MatFieldProperties } from '../models';
+
+export type FileUploadFieldConfig = CrisprControlConfig & MatFieldProperties & {
   controlType: ControlType.FILE_UPLOAD;
-  uploadFile?: (parentGroup: FormGroup, files:FileList, uploadComponent: FileUploadComponent) => Promise<unknown> | unknown;
   filesChanged?: (parentGroup: FormGroup, files: FileList) => void;
   allowMultipleFiles?: boolean;
   showDeleteButton?: boolean;
-  showUploadButton?: boolean;
   acceptedTypes?: string;
   selectButtonText?: string;
-  uploadButtonText?: string;
   selectFilesButtonType?: ButtonType;
+  // isolate possible configurations regarding upload to for user to properly type configuration
+} & (EnabledUploadButtonConfig | DisabledUploadButtonConfig);
+
+//
+interface EnabledUploadButtonConfig {
+  uploadFiles: (parentGroup: FormGroup, files:FileList, uploadComponent: FileUploadComponent) => Promise<unknown> | unknown;
+  showUploadProgress?: boolean;
+  disableAfterUpload?: boolean;
   uploadButtonType?: ButtonType;
+  uploadButtonText?: string;
 }
+
+type DisabledUploadButtonConfig = NeverProps<EnabledUploadButtonConfig>;
+
+/**
+ * Maps all property types to never.
+ * Useful for using discriminating unions to refine properties
+ * */
+type NeverProps<T> = { [P in keyof T]: never; };
+
