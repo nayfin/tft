cbe3ebca4709f4be28cca84c5cd50a0c443cd985
nayfin <natjkern@gmail.com>
2021-04-16 10:37:25 -0500

    cleanup

--- libs/interact/src/lib/directives/drag-preview.directive.ts
+++ libs/interact/src/lib/directives/drag-preview.directive.ts
@@ -3,12 +3,11 @@ import {Directive, InjectionToken, Input, TemplateRef} from '@angular/core';
 export const TFT_DRAG_PREVIEW = new InjectionToken<DragPreviewDirective>('DragPreviewDirective');
 
 @Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
   selector: 'ng-template[tftDragPreview]',
-  // providers: [{provide: TFT_DRAG_PREVIEW, useExisting: DragPreviewDirective}]
-})
+ })
 export class DragPreviewDirective<T = any> {
 
   @Input() dragData: T;
   constructor(public templateRef: TemplateRef<T>) { }
-
 }
