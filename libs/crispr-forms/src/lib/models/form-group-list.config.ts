6534a7d41e4f69adcecd512c5bbc73804eb8c2df
nayfin <natjkern@gmail.com>
2021-07-18 16:23:48 -0500

    deprecates controlName in GroupListItemConfig

--- libs/crispr-forms/src/lib/models/form-group-list.config.ts
+++ libs/crispr-forms/src/lib/models/form-group-list.config.ts
@@ -1,9 +1,16 @@
-import { CrisprControlConfig, FormConfig, ControlType, ControlValue } from './';
+import { CrisprControlConfig, ControlType } from './';
 import { SubGroupConfig } from './crispr-field.config';
+
+export type GroupListItemConfig = Omit<SubGroupConfig, 'controlName'> & {
+  /**
+   * @deprecated controlName doesn't do anything in the itemConfig and should be removed
+   *  */
+  controlName?: string;
+}
 
 export interface FormGroupListConfig extends CrisprControlConfig {
   controlType: ControlType.GROUP_LIST;
-  itemConfig: SubGroupConfig;
+  itemConfig: GroupListItemConfig;
   addItemLabel?: string;
   itemLabelBuilder?: (index: number) => string;
   minListLength?: number;
