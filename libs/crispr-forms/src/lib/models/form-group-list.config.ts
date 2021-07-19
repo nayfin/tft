5551b4c50ce452402214748602313a78f5ee2042
nayfin <natjkern@gmail.com>
2021-07-19 18:57:31 -0500

    interact release:   "version": "12.2.3",

--- libs/crispr-forms/src/lib/models/form-group-list.config.ts
+++ libs/crispr-forms/src/lib/models/form-group-list.config.ts
@@ -1,16 +1,17 @@
 import { CrisprControlConfig, ControlType } from './';
 import { SubGroupConfig } from './crispr-field.config';
 
-export type GroupListItemConfig = Omit<SubGroupConfig, 'controlName'> & {
-  /**
-   * @deprecated controlName doesn't do anything in the itemConfig and should be removed
-   *  */
-  controlName?: string;
-}
+// TODO: Below type would be ideal type to use for itemConfig, but there were some issues getting it to work
+// export type GroupListItemConfig = Omit<SubGroupConfig, 'controlName'> & {
+//   /**
+//    * @deprecated controlName doesn't do anything in the itemConfig and should be removed
+//    *  */
+//   controlName?: string;
+// }
 
 export interface FormGroupListConfig extends CrisprControlConfig {
   controlType: ControlType.GROUP_LIST;
-  itemConfig: GroupListItemConfig;
+  itemConfig: SubGroupConfig; // TODO: should be GroupListItemConfig but having issues working it out
   addItemLabel?: string;
   itemLabelBuilder?: (index: number) => string;
   minListLength?: number;
