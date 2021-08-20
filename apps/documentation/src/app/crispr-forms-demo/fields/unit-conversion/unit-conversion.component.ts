e56ede2a75303f74df5f2f18994d5c781ce8d2e7
nayfin <natjkern@gmail.com>
2021-08-20 14:35:21 -0500

    remove debugging configurations from unit conversions example config

--- apps/documentation/src/app/crispr-forms-demo/fields/unit-conversion/unit-conversion.component.ts
+++ apps/documentation/src/app/crispr-forms-demo/fields/unit-conversion/unit-conversion.component.ts
@@ -76,53 +76,6 @@ export class UnitConversionComponent {
         }
       },
       {
-        controlType: ControlType.SUB_GROUP,
-        controlName: 'subGroupedConvertor',
-        fields: [
-          {
-            controlType: ControlType.UNIT_CONVERSION,
-            label: 'Unit Conversion Field without unit selection field',
-            controlName: 'withoutUnitSelection',
-            showUnitSelect: false,
-            validators: [Validators.required, Validators.max(3)],
-            fieldSuffix: 'feet',
-            initialDisplayValueConversion: (value) => {
-              if (value) {
-                const initialQty = qty(`${value} m`).to('ft').scalar;
-                console.log('initialDisplayValueConversion', initialQty, value);
-                return initialQty;
-              } else {
-                return 0;
-              }
-            },
-            storedValueConversion: (value) => {
-              const num = parseFloat(value);
-              const storedValue = qty(`${value} ft`).to('m').scalar;
-              console.log('storedValueConversion', num, storedValue);
-              return storedValue;
-            }
-          },
-          {
-            controlType: ControlType.AUTOCOMPLETE,
-            label: 'Autocomplete Test',
-            controlName: 'autocompleteTest',
-            validators: [Validators.required, Validators.max(3)],
-            options: (group, searchTerm) => {
-              return [
-              {label: 'abc', value: 'abc'},
-              {label: 'efg', value: 'efg'}
-              ].filter(option => option.label.includes(searchTerm))
-            }
-          },
-          {
-            controlType: ControlType.INPUT,
-            label: 'Input Test',
-            controlName: 'inputTest',
-            validators: [Validators.required]
-          },
-        ]
-      },
-      {
         controlType: ControlType.BUTTON,
         label: 'SUBMIT',
         callback: (group) => { console.log({value: group.value})}
