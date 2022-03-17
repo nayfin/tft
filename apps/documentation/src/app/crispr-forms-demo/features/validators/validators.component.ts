b6bb9de669b68546b20702a1b3196a3f1c07a983
Nathan <natjkern@gmail.com>
2022-03-17 12:49:43 -0500

    fix bug in someControlIsValid validator

--- apps/documentation/src/app/crispr-forms-demo/features/validators/validators.component.ts
+++ apps/documentation/src/app/crispr-forms-demo/features/validators/validators.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormConfig, ControlType } from '@tft/crispr-forms';
 import { someControlIsValid } from '@tft/form-validation-handler';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'tft-validators',
@@ -24,6 +25,30 @@ export class ValidatorsComponent {
         },
         fieldSuffix: 'meters',
         color: 'primary',
+      },
+      {
+        controlType: ControlType.INPUT,
+        inputType: 'number',
+        controlName: 'conditionallyDisabledField',
+        validators: [Validators.required],
+        label: 'Conditionally Disabled Field',
+        heading: {
+          label: `'primary'`,
+          typographyClass: 'mat-h2'
+        },
+        fieldSuffix: 'meters',
+        color: 'primary',
+        hideDisabled: true,
+        disabledCallback: (group) => {
+          const lengthControl = group.get('length') as FormControl;
+          return lengthControl.valueChanges.pipe(
+            map((lengthVal: number) => {
+              const invalid = !(lengthVal && lengthVal > 1)
+              return invalid;
+            })
+          )
+
+        }
       },
       {
         controlType: ControlType.INPUT,
--- libs/form-validation-handler/src/lib/validators/some-control-is-valid.ts
+++ libs/form-validation-handler/src/lib/validators/some-control-is-valid.ts
@@ -3,10 +3,7 @@ import { FormGroup, ValidatorFn } from '@angular/forms';
 export function someControlIsValid(controlNames: string[]): ValidatorFn {
   return (formGroup: FormGroup) => {
     const controls = controlNames.map(controlName => formGroup.get(controlName));
-    const someControlIsValid = controls.some((control) => {
-      control.updateValueAndValidity({onlySelf: true});
-      return control.valid;
-    });
+    const someControlIsValid = controls.some((control) => control.valid);
     if (someControlIsValid) {
       controls.forEach(control => {
         // mark all controls as valid
