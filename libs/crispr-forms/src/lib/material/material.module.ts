7a15d9b9a8520ff684f9e3b69f9abef62f3e74b6
Nathan <natjkern@gmail.com>
2022-06-24 22:48:20 -0500

    creates module for radio field and removes

--- libs/crispr-forms/src/lib/material/material.module.ts
+++ libs/crispr-forms/src/lib/material/material.module.ts
@@ -2,14 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-
 import { FormValidationHandlerModule } from '@tft/form-validation-handler';
 
 import { FieldContainerModule } from '../field-container/field-container.component';
@@ -29,25 +21,13 @@ import { DividerModule } from './divider/divider.component';
 // tslint:disable-next-line: max-line-length
 import { AutocompleteChiplistFieldModule } from './autocomplete-chiplist-field/autocomplete-chiplist-field.component';
 import { OptionModule } from './option/option.component';
-import { RadioFieldComponent } from './radio-field/radio-field.component';
+import { RadioFieldModule } from './radio-field/radio-field.component';
 import { UnitConversionFieldModule } from './unit-conversion-field/unit-conversion-field.component';
 import { FileUploadFieldModule } from './file-upload';
 
 // TODO: Should we import UI library?
 // Will the tree shaker shake the unused modules?
-const MAT_DESIGN_MODULES = [
-  MatIconModule,
-  MatRadioModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatTooltipModule,
-  MatFormFieldModule,
-  MatProgressBarModule,
-];
 
-const CRISPR_FIELDS = [
-  RadioFieldComponent,
-];
 @NgModule({
   imports: [
     CommonModule,
@@ -61,6 +41,7 @@ const CRISPR_FIELDS = [
     DatepickerFieldModule,
     InputFieldModule,
     FileUploadFieldModule,
+    RadioFieldModule,
     SliderFieldModule,
     TextareaFieldModule,
     UnitConversionFieldModule,
@@ -69,16 +50,7 @@ const CRISPR_FIELDS = [
     HeadingModule,
     FieldContainerModule,
     OptionModule,
-    ...MAT_DESIGN_MODULES,
-  ],
-  exports: [
-    ...MAT_DESIGN_MODULES,
-    ...CRISPR_FIELDS
-  ],
-  declarations: [
-    ...CRISPR_FIELDS,
   ],
 })
-
 export class MaterialModule {
 }
