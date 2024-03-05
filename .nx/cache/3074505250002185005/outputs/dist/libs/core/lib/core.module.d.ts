51a505ef279d28d340f91890fea55adca927e435
nayfin <natjkern@gmail.com>
2024-03-04 22:53:44 -0600

    core release:   "version": "16.0.0",

---
+++ .nx/cache/3074505250002185005/outputs/dist/libs/core/lib/core.module.d.ts
@@ -0,0 +1,16 @@
+import * as i0 from "@angular/core";
+import * as i1 from "./header/header.component";
+import * as i2 from "./sidenav-list/sidenav-list.component";
+import * as i3 from "@angular/common";
+import * as i4 from "@angular/material/sidenav";
+import * as i5 from "@angular/material/toolbar";
+import * as i6 from "@angular/material/icon";
+import * as i7 from "@angular/material/expansion";
+import * as i8 from "@angular/material/tooltip";
+import * as i9 from "@angular/material/list";
+import * as i10 from "@angular/material/button";
+export declare class CoreModule {
+    static ɵfac: i0.ɵɵFactoryDeclaration<CoreModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<CoreModule, [typeof i1.HeaderComponent, typeof i2.SidenavListComponent], [typeof i3.CommonModule, typeof i4.MatSidenavModule, typeof i5.MatToolbarModule, typeof i6.MatIconModule, typeof i7.MatExpansionModule, typeof i8.MatTooltipModule, typeof i9.MatListModule, typeof i10.MatButtonModule], [typeof i1.HeaderComponent, typeof i2.SidenavListComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<CoreModule>;
+}
---
+++ .nx/cache/3074505250002185005/outputs/dist/libs/core/lib/public_api.d.ts
@@ -0,0 +1,4 @@
+export * from './core.module';
+export * from './sidenav-list/sidenav-list.model';
+export * from './header/header.component';
+export * from './sidenav-list/sidenav-list.component';
