8afb4b8f0698f5659570e451a04485edab92dfcf
nayfin <natjkern@gmail.com>
2024-03-11 21:37:15 -0500

    removes .nx

--- .nx/cache/16193250569606612056/outputs/dist/libs/core/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/public_api';
-export * from './lib/sidenav-list/sidenav-list.model';
--- .nx/cache/16193250569606612056/outputs/dist/libs/core/lib/public_api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './core.module';
-export * from './sidenav-list/sidenav-list.model';
-export * from './header/header.component';
-export * from './sidenav-list/sidenav-list.component';
